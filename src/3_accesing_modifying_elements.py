867a785d2d036e0b7e0cf3e2c8a18bf817127069
djeada <addjellouli1@gmail.com>
2021-05-08 19:22:15 +0200

    updated searching

--- src/3_accesing_modifying_elements.py
+++ src/3_accesing_modifying_elements.py
@@ -11,37 +11,37 @@ def main():
     - Selecting elements by condition is possible.
     """
 
-    array_1 = np.array([0, 1, 2, 3, 4, 5])
+    array = np.array([0, 1, 2, 3, 4, 5])
 
-    assert array_1[0] == 0
-    assert array_1[-1] == 5
-    assert (array_1[1:3] == np.array([1, 2])).all()
+    assert array[0] == 0
+    assert array[-1] == 5
+    assert (array[1:3] == np.array([1, 2])).all()
 
-    array_1 = np.append(array_1, 6)
-    array_1 = np.insert(array_1, 1, 100)
+    array = np.append(array, 6)
+    array = np.insert(array, 1, 100)
 
-    assert array_1[-1] == 6
-    assert array_1[1] == 100
+    assert array[-1] == 6
+    assert array[1] == 100
 
-    array_1[1] = 20
+    array[1] = 20
 
-    assert array_1[1] == 20
+    assert array[1] == 20
 
-    array_1[:5] += 1
+    array[:5] += 1
 
-    assert (array_1 == np.array([1, 21, 2, 3, 4, 4, 5, 6])).all()
+    assert (array == np.array([1, 21, 2, 3, 4, 4, 5, 6])).all()
 
-    array_1 = array_1 / 2
+    array = array / 2
 
-    assert (array_1 == np.array([0.5, 10.5, 1.0, 1.5, 2.0, 2.0, 2.5, 3.0])).all()
+    assert (array == np.array([0.5, 10.5, 1.0, 1.5, 2.0, 2.0, 2.5, 3.0])).all()
 
-    array_1 = array_1 % 3
+    array = array % 3
 
-    assert (array_1 == np.array([0.5, 1.5, 1.0, 1.5, 2.0, 2.0, 2.5, 0.0])).all()
+    assert (array == np.array([0.5, 1.5, 1.0, 1.5, 2.0, 2.0, 2.5, 0.0])).all()
 
-    array_1 = np.delete(array_1, (1, 3))
+    array = np.delete(array, (1, 3))
 
-    assert (array_1 == np.array([0.5, 1.0, 2.0, 2.0, 2.5, 0.0])).all()
+    assert (array == np.array([0.5, 1.0, 2.0, 2.0, 2.5, 0.0])).all()
 
 
 if __name__ == "__main__":
