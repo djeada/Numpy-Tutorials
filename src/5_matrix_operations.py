638b536697d4a9357a9ad011204479df26fb1749
djeada <addjellouli1@gmail.com>
2021-05-09 00:37:47 +0200

    updated matrix operations

--- src/5_matrix_operations.py
+++ src/5_matrix_operations.py
@@ -1,87 +1,37 @@
 import numpy as np
 
 
-def increase_by_k(array, k):
-    return array + k
-
-
-def decrease_by_k(array, k):
-    return array - k
-
-
-def multiply_by_k(array, k):
-    return array * k
-
-
-def divide_by_k(array, k):
-    return array / k
-
-
-def elements_greater_than_k(array, k):
-    return array > k
-
-
-def elements_less_than_k(array, k):
-    return array < k
-
-
-def sqrt_of_each_element(array):
-    return np.sqrt(array)
-
-
-def sum_two(arr_1, arr_2):
-    return arr_1 + arr_2
-
-
-def difference_two(arr_1, arr_2):
-    return arr_1 - arr_2
-
-
-# my_3D_array / my_vector
-
-# my_3D_array % my_vector
-
-
 def main():
 
     """ 
-    - Find index of a given value.
-    - Find max and min.
-    - Unique elements.
-    - Histogram.
+    Basic matrix operations are supported by numpy.
+    Addition, subtraction, multiplication, division by:
+    - scalar
+    - vector 
+    - matrix
     """
 
-    array_1 = np.array([0, 1, 2, 3, 4, 5])
+    matrix_A = np.array([[3, 9], [21, -3]])
 
-    assert array_1[0] == 0
-    assert array_1[-1] == 5
-    assert (array_1[1:3] == np.array([1, 2])).all()
+    assert (matrix_A + 5 == np.array([[8, 14], [26, 2]])).all()
+    assert (matrix_A - 1 == np.array([[2, 8], [20, -4]])).all()
+    assert (matrix_A * 2 == np.array([[6, 18], [42, -6]])).all()
+    assert (matrix_A / 3 == np.array([[1, 3], [7, -1]])).all()
 
-    array_1 = np.append(array_1, 6)
-    array_1 = np.insert(array_1, 1, 100)
+    vector = [1, 2]
+    assert (matrix_A + vector == np.array([[4, 11], [22, -1]])).all()
+    assert (matrix_A - vector == np.array([[2, 7], [20, -5]])).all()
+    assert (matrix_A * vector == np.array([[3, 18], [21, -6]])).all()
+    assert (matrix_A / vector == np.array([[3, 4.5], [21, -1.5]])).all()
 
-    assert array_1[-1] == 6
-    assert array_1[1] == 100
+    matrix_B = np.array([[4, 9], [25, 16]])
 
-    array_1[1] = 20
+    assert (np.sqrt(matrix_B) == np.array([[2, 3], [5, 4]])).all()
 
-    assert array_1[1] == 20
-
-    array_1[:5] += 1
-
-    assert (array_1 == np.array([1, 21, 2, 3, 4, 4, 5, 6])).all()
-
-    array_1 = array_1 / 2
-
-    assert (array_1 == np.array([0.5, 10.5, 1.0, 1.5, 2.0, 2.0, 2.5, 3.0])).all()
-
-    array_1 = array_1 % 3
-
-    assert (array_1 == np.array([0.5, 1.5, 1.0, 1.5, 2.0, 2.0, 2.5, 0.0])).all()
-
-    array_1 = np.delete(array_1, (1, 3))
-
-    assert (array_1 == np.array([0.5, 1.0, 2.0, 2.0, 2.5, 0.0])).all()
+    assert (matrix_A + matrix_B == np.array([[7, 18], [46, 13]])).all()
+    assert (matrix_A - matrix_B == np.array([[-1, 0], [-4, -19]])).all()
+    assert (matrix_A * matrix_B == np.array([[12, 81], [525, -48]])).all()
+    assert (matrix_B / matrix_A == np.array([[4 / 3, 1], [25 / 21, -16 / 3]])).all()
 
 
 if __name__ == "__main__":
