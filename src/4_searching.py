26b4f6220be7adfb8e56be248a4c182d8b2773e6
djeada <addjellouli1@gmail.com>
2021-05-08 19:22:15 +0200

    updated searching

--- src/4_searching.py
+++ src/4_searching.py
@@ -1,62 +1,49 @@
 import numpy as np
 
 
-def max_value(array):
-    return array.max()
-
-
-def min_value(array):
-    return array.min()
-
-
-def max_index(array):
-    return array.argmax()
-
-
-def min_index(array):
-    return array.argmin()
-
-
 def main():
 
     """ 
     - Find index of a given value.
+    - Find indices of all values meeting given condition.
     - Find max and min.
     - Unique elements.
     - Histogram.
     """
 
-    array_1 = np.array([0, 1, 2, 3, 4, 5])
+    array = np.array([0, 1, 2, 3, 4, 5])
+    number = 2
+    result = np.where(array == 2)
 
-    assert array_1[0] == 0
-    assert array_1[-1] == 5
-    assert (array_1[1:3] == np.array([1, 2])).all()
+    assert array[result[0]] == number
+    assert array.max() == 5
+    assert array.min() == 0
+    assert array[array.argmax()] == 5
+    assert array[array.argmin()] == 0
 
-    array_1 = np.append(array_1, 6)
-    array_1 = np.insert(array_1, 1, 100)
+    for i in np.where((array > 1) & (array < 4))[0]:
+        num = array[i]
+        assert num > 1 and num < 4
 
-    assert array_1[-1] == 6
-    assert array_1[1] == 100
+    array_2D = np.array([[0, 1], [1, 1], [5, 9]])
+    number = 1
+    result = np.where(array_2D == number)
 
-    array_1[1] = 20
+    for cor in list(zip(result[0], result[1])):
+        assert array_2D[cor[0]][cor[1]] == number
 
-    assert array_1[1] == 20
+    assert array_2D.max() == 9
+    assert array_2D.min() == 0
 
-    array_1[:5] += 1
+    result = np.unique(array_2D)
 
-    assert (array_1 == np.array([1, 21, 2, 3, 4, 4, 5, 6])).all()
+    assert (result == [0, 1, 5, 9]).all()
 
-    array_1 = array_1 / 2
+    unique, freq = np.unique(array_2D, return_counts=True)
+    histogram = dict(zip(unique, freq))
+    expected_result = {0: 1, 1: 3, 5: 1, 9: 1}
 
-    assert (array_1 == np.array([0.5, 10.5, 1.0, 1.5, 2.0, 2.0, 2.5, 3.0])).all()
-
-    array_1 = array_1 % 3
-
-    assert (array_1 == np.array([0.5, 1.5, 1.0, 1.5, 2.0, 2.0, 2.5, 0.0])).all()
-
-    array_1 = np.delete(array_1, (1, 3))
-
-    assert (array_1 == np.array([0.5, 1.0, 2.0, 2.0, 2.5, 0.0])).all()
+    assert histogram == expected_result
 
 
 if __name__ == "__main__":
