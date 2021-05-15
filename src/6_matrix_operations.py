0cfe7db59c83390aaf2e8a3e9a6aacfdd1589085
djeada <addjellouli1@gmail.com>
2021-05-15 02:30:25 +0200

    added examples with vectors

--- src/6_matrix_operations.py
+++ src/6_matrix_operations.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# from numpy.linalg import det, inv
+
 
 def main():
 
@@ -32,6 +34,27 @@ def main():
     assert (matrix_A - matrix_B == np.array([[-1, 0], [-4, -19]])).all()
     assert (matrix_A * matrix_B == np.array([[12, 81], [525, -48]])).all()
     assert (matrix_B / matrix_A == np.array([[4 / 3, 1], [25 / 21, -16 / 3]])).all()
+
+    """
+    The transpose of a matrix is a reversal of its rows with its columns.
+    Square matrices have determinants.
+    The identity matrix is a square matrix with ones on the diagonal and zeros elsewhere. 
+    The inverse of a square matrix M is a matrix of the same size, N, such that M⋅N=I.
+    """
+    M = np.array([[-4, 5], [1, 7], [8, 3]])
+    N = np.array([[3, -5, 2, 7], [-5, 1, -4, -3]])
+    print(np.dot(M, N))
+
+    assert (np.transpose(matrix_A) == np.array([[3, 21], [9, -3]])).all()
+    assert (np.transpose(matrix_B) == np.array([[4, 25], [9, 16]])).all()
+
+    assert np.linalg.det(matrix_A) == -198
+    assert abs(np.linalg.det(matrix_B) + 161) < 0.001
+
+    identity = np.eye(2)
+    inverse = np.linalg.inv(matrix_A)
+    product = np.dot(matrix_A, inverse)
+    assert (product == identity).all()
 
 
 if __name__ == "__main__":
