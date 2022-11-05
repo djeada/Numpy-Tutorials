866bff1f72eb1fc301e53551f670a3ed991424a9
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-11-05 22:39:49 +0100

    Update 3_vector_operations.py

--- src/3_vector_operations.py
+++ src/3_vector_operations.py
@@ -4,34 +4,65 @@ import math
 
 def main():
 
-    """
-    Operations:
-    - vector addition
-    - scalar multiplication
-    - dot product
-    - the cross product
-    - the angle between two vectors:
-    """
-
     vector_row = np.array([[1, 2, 3, 4]])
+    print(vector_row)
+    print(vector_row.shape)
+    print()
+
     vector_column = np.array([[1], [2], [3], [4]])
+    print(vector_column)
+    print(vector_column.shape)
+    print()
 
-    assert vector_row.shape == (1, 4)
-    assert vector_column.shape == (4, 1)
+    vector_row_transposed = vector_row.T
+    print(vector_row_transposed)
+    print()
 
-    assert (vector_row.T == vector_column).all()
-    assert (vector_column.T == vector_row).all()
+    vector_column_transposed = vector_column.T
+    print(vector_column_transposed)
+    print()
 
-    assert np.linalg.norm(vector_row, 1) == 4.0
-    assert math.isclose(np.linalg.norm(vector_row, 2), 5.5, rel_tol=1e-2)
-    assert np.linalg.norm(vector_row, np.inf) == 10.0
+    # norms
+    first_norm = np.linalg.norm(vector_row, 1)
+    print(first_norm)
+
+    second_norm = np.linalg.norm(vector_row, 2)
+    print(second_norm)
+
+    inf_norm = np.linalg.norm(vector_row, np.inf)
+    print(inf_norm)
+
+    # addition of vectors
+    vector_sum = vector_row + vector_row
+    print(vector_sum)
+    print()
+
+    scalar_sum = vector_row + 5
+    print(scalar_sum)
+    print()
+
+    # scalar multiplication
+    scalar_multiplication = 3 * vector_row
+    print(scalar_multiplication)
+    print()
+
+    # dot product
+    dot_product = np.dot(vector_row, vector_row.T)
+    print(dot_product)
+    print()
 
     v = np.array([[9, 2, 5]])
     w = np.array([[-3, 8, 2]])
-    theta = np.arccos(np.dot(v, w.T) / (np.linalg.norm(v) * np.linalg.norm(w)))
 
-    assert math.isclose(theta[0][0], 1.582, rel_tol=1e-2)
-    assert (np.cross(v, w) == [[-36, -33, 78]]).all()
+    # cross product
+    cross_product = np.cross(v, w)
+    print(cross_product)
+    print()
+
+    # the angle between two vectors
+    theta = np.arccos(np.dot(v, w.T) / (np.linalg.norm(v) * np.linalg.norm(w)))
+    print(theta)
+    print()
 
 
 if __name__ == "__main__":
