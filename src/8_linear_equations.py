96f246219aab0fde6b2ba87953b78ac2fb6caac8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-11-06 14:33:10 +0100

    Update 8_linear_equations.py

--- src/8_linear_equations.py
+++ src/8_linear_equations.py
@@ -4,30 +4,13 @@ from numpy.linalg import solve
 
 def main():
 
-    """
-    You can solve linear equations from definition.
-    y = A*x
-    x = A^-1*y
-    or using numpy solve method.
-    """
-
     matrix = np.matrix([[2, 0, 5], [3, 4, 8], [2, 7, 3]])
     inverse = matrix.I
 
     y = np.matrix([[10], [15], [5]])
 
     solution = inverse * y
-
-    assert np.allclose(matrix * solution - y, np.array([[0, 0, 0]]))
-    assert np.allclose(solution, solve(matrix, y))
-
-    # Eigenvalues and Eigenvectors
-    A = np.array([[1, 2], [2, 1]])
-    x = np.array([1, 0])
-
-    for i in range(10):
-        x = A @ x
-
+    print(solution)
 
 if __name__ == "__main__":
     main()
